0fb2e6cb08d05c772d38e35deff97071b0c993e3
Дмитрий Загузин <cindersstone@gmail.com>
2021-04-02 06:32:20 +0300

    Update TilesView.java
    накидал тудушек

--- TilesView.java
+++ TilesView.java
@@ -12,12 +12,14 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 class Tile{
+    //TODO: зачем столько строк
     int color;
     int left;
     int right;
     int top;
     int bottom;
-
+    
+    //TODO: Такой конструктор класса генерируется автоматически и писать его бессмысленно
     Tile(){
 
     }
@@ -29,17 +31,19 @@ class Tile{
         bottom = b;
         top = t;
     }
-
+    
+    //TODO: для красоты нужно указывать индедификатор доступа
     int getColor(){
         return color;
     }
-
+    //TODO: Аналагично
     void setColor(int c){
         color = c;
     }
 }
 
 public class TilesView extends View {
+    //TODO: Можно объявить все в одной строке
     boolean flag = true;
     int rowNum = 4;
     int colNum = 4;
@@ -63,7 +67,8 @@ public class TilesView extends View {
     public void onDraw(Canvas canvas){
         width = canvas.getWidth();
         height = canvas.getHeight();
-
+        
+        //TODO: Пробелы между оператором деления
         int t_width = width/rowNum;
         int t_height = height/colNum;
 
@@ -77,16 +82,20 @@ public class TilesView extends View {
 
         for(int i = 0; i < rowNum; i++){
             for (int j = 0; j < colNum; j++) {
+                //TODO: Пробелы между оператором деления
                 int left = j*t_width;
                 int top = i*t_height;
                 int right = left + t_width;
                 int bottom = top + t_height;
 
                 Rect tile = new Rect();
+                //TODO: Пробелы
                 tile.set(left+outline, top+outline, right-outline, bottom-outline);
-
+                
                 int color;
                 if (flag) {
+                    
+                    //TODO: Два раза идет бессмысленная отрисовка одного и того же
                     if (Math.random() > 0.5){
                         canvas.drawRect(tile, light);
                         color = 1;
@@ -103,6 +112,7 @@ public class TilesView extends View {
                     tiles[i][j] = new Tile(left, top, right, bottom, color);
                 } else {
                     color = tiles[i][j].getColor();
+                    //TODO: Тут не нужно менять цвет? Так же можно сделать тернарник
                     if (color == 0){
                         canvas.drawRect(tile, light);
                     } else {
@@ -112,6 +122,7 @@ public class TilesView extends View {
                 }
             }
         }
+        //TODO: flag = !flag
         if (flag == true) {
             flag = false;
         }
@@ -127,7 +138,8 @@ public class TilesView extends View {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             for (int i = 0; i < rowNum; i++) {
                 for (int j = 0; j < colNum; j++){
-
+                    
+                    //TODO: Запихать все в один if
                     if (tiles[i][j].left < x && tiles[i][j].right > x){
                         if (tiles[i][j].top < y && tiles[i][j].bottom > y){
 
@@ -137,6 +149,7 @@ public class TilesView extends View {
                                 for (int jj = 0; jj < colNum; jj++) {
 
                                     if (ii == k || jj == m){
+                                        //TODO: tiles[ii][jj].setColor((tiles[ii][jj].getColot() + 1) % 2);
                                         if (tiles[ii][jj].getColor() == 1)
                                             tiles[ii][jj].setColor(0);
                                         else
